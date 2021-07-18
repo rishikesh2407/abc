4c7050d7d37dfb1adc33afab3b2a41ad2e425555
rishikesh2407 <79324439+rishikesh2407@users.noreply.github.com>
2021-07-18 18:11:59 +0530

    Delete dictonary1.py

--- dictonary1.py
+++
@@ -1,8 +0,0 @@
-dict={
-    "pankha":"fan",
-    "kitab":"notebook",
-    "dabba":"box"
-}
-print("options are", dict.keys())
-a=input("Enter your Hindi word: ")
-print("English translation of your hindi word is:",dict.get(a))
